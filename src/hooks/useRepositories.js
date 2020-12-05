d0d7a697f4ab23d3ab339838863bafdf45f4a512
shallowse <noemail>
2020-12-05 12:47:26 +0000

    part10: 10.11.

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { loading, error, data } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  return { data, loading, error };
+};
+
+export default useRepositories;
